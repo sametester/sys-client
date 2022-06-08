77b57f17a5fe851541390aab113e2b424c50ebb8
sametester <sittichok.boo@gmail.com>
2022-06-08 11:53:29 +0700

    success

--- src/index.js
+++ src/index.js
@@ -12,16 +12,16 @@ import AuthContextProvider from './contexts/AuthContext';
 import ErrorContextProvider from './contexts/ErrorContext';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <ErrorContextProvider>
-                <AuthContextProvider>
-                    <App />
-                </AuthContextProvider>
-            </ErrorContextProvider>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <BrowserRouter>
+      <ErrorContextProvider>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </ErrorContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
